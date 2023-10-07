09ee18f654991535ca58581997bb2aeffafc8222
Jisoo Park <xxxyel@gmail.com>
2023-10-07 22:05:42 +0900

    Defˆault

--- zio-http-docs/src/main/scala/zio/http/docs/GenConfigDocs2.scala
+++ zio-http-docs/src/main/scala/zio/http/docs/GenConfigDocs2.scala
@@ -4,7 +4,13 @@ import zio.{Chunk, Config}
 
 import scala.collection.View
 
-case class Row(name: String, tpe: String, description: Option[String] = None, optional: Boolean = false)
+case class Row(
+  name: String,
+  tpe: String,
+  description: Option[String] = None,
+  optional: Boolean = false,
+  default: Option[Any] = None,
+)
 
 case class Table(rows: Chunk[Row]) {
   def toDocusaurusMarkdown: String = {
@@ -48,6 +54,10 @@ trait Cursor {
         Cursor.GenericCursor(left, this :: history).downFields ++
           Cursor.GenericCursor(right, this :: history).downFields
 
+      case f: Config.Fallback[_] =>
+        // Second will be matched by `upFallback` later
+        Cursor.GenericCursor(f.first, this :: history).downFields
+
       case _: Config.Primitive[_] => Chunk.empty
       case _                      => Chunk.empty
     }
@@ -80,13 +90,26 @@ trait Cursor {
   def upUntilSiblings: View[Cursor] =
     history.view.takeWhile(!_.isZipped)
 
-  def descriptions: Chunk[String] =
+  def getDescriptions: Chunk[String] =
     upUntilSiblings
       .map(_.config)
       .collect { case Config.Described(_, desc) =>
         desc
       }
       .to(Chunk)
+
+  def upFallback: Option[Cursor.FallbackCursor] =
+    upUntilSiblings.collectFirst {
+      case c if c.config.isInstanceOf[Config.Fallback[_]] =>
+        Cursor.FallbackCursor(c.config.asInstanceOf[Config.Fallback[_]], c.history)
+    }
+
+  def downConstant: Option[Cursor.ConstantCursor] =
+    config match {
+      case c: Config.Constant[_] => Some(Cursor.ConstantCursor(c, history))
+      case Config.Lazy(thunk)    => Cursor.fromConfig(thunk()).downConstant
+      case _                     => None
+    }
 
   def toTable: Table = Table(downFields.map(_.toRow))
 }
@@ -99,23 +122,34 @@ object Cursor {
 
     def toRow: Row = {
       val optional = upUntilSiblings.exists(_.isOptional)
+      val default  = upFallback.flatMap(_.getDefault)
+      val tpe      = downPrimitive.map(_.getType).getOrElse("Unknown")
 
-      Row(name = name, tpe = tpe, description = descriptions.headOption, optional = optional)
+      Row(name = name, tpe = tpe, description = getDescriptions.headOption, optional = optional, default = default)
     }
-
-    def tpe: String =
-      this.downPrimitive match {
-        case Some(p) => p.tpe
-        case None    => "Unknown"
-      }
 
   }
 
   case class PrimitiveCursor(config: Config.Primitive[_], history: List[Cursor]) extends Cursor {
-    def tpe: String = config match {
+    def getType: String = config match {
       case Config.Constant(_) => "Constant"
       case other              => other.toString.replaceAll("Type$", "")
     }
+  }
+
+  case class FallbackCursor(config: Config.Fallback[_], history: List[Cursor]) extends Cursor {
+
+    private val missingOnly     = Config.Error.MissingData(Chunk.empty, "")
+    def getDefault: Option[Any] =
+      config match {
+        case Config.FallbackWith(_, v, cond) if cond(missingOnly) => Cursor.fromConfig(v).downConstant.map(_.getValue)
+        case _                                                    => None
+      }
+
+  }
+
+  case class ConstantCursor(config: Config.Constant[_], history: List[Cursor]) extends Cursor {
+    def getValue: Any = config.value
   }
 
   def fromConfig(config: Config[_]): Cursor = GenericCursor(config, Nil)
--- zio-http-docs/src/test/scala/zio/http/docs/GenConfigDocs2Spec.scala
+++ zio-http-docs/src/test/scala/zio/http/docs/GenConfigDocs2Spec.scala
@@ -6,104 +6,127 @@ import zio.test._
 object GenConfigDocs2Spec extends ZIOSpecDefault {
 
   def spec = suite("GenConfigDocs")(
-    test("Single") {
-      val conf     = Config.string("str")
-      val expected =
-        Table(
-          Chunk(
-            Row("str", "Text"),
-          ),
-        )
+    suite("basic")(
+      test("Single") {
+        val conf     = Config.string("str")
+        val expected =
+          Table(
+            Chunk(
+              Row("str", "Text"),
+            ),
+          )
 
-      assertTrue(GenConfigDocs2.gen(conf) == expected)
-    },
-    test("Zipped") {
-      val conf =
-        Config.string("str") ++ Config.int("num")
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+      test("Zipped") {
+        val conf =
+          Config.string("str") ++ Config.int("num")
 
-      val expected = {
-        Table(
-          Chunk(
-            Row("str", "Text"),
-            Row("num", "Integer"),
-          ),
-        )
-      }
-
-      assertTrue(GenConfigDocs2.gen(conf) == expected)
-    },
-    test("Zipped and Mapped") {
-      case class Foo(str: String, num: Int)
-      val conf =
-        (Config.string("str") ++ Config.int("num")).map { case (str, num) =>
-          Foo(str, num)
+        val expected = {
+          Table(
+            Chunk(
+              Row("str", "Text"),
+              Row("num", "Integer"),
+            ),
+          )
         }
 
-      val expected = {
-        Table(
-          Chunk(
-            Row("str", "Text"),
-            Row("num", "Integer"),
-          ),
-        )
-      }
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+      test("Zipped and Mapped") {
+        case class Foo(str: String, num: Int)
+        val conf =
+          (Config.string("str") ++ Config.int("num")).map { case (str, num) =>
+            Foo(str, num)
+          }
 
-      assertTrue(GenConfigDocs2.gen(conf) == expected)
-    },
-    test("Described") {
-      case class Foo(str: String, num: Int)
-      val conf =
-        (Config.string("str") ++ Config.int("num").??("it's a number")).map { case (str, num) =>
-          Foo(str, num)
+        val expected = {
+          Table(
+            Chunk(
+              Row("str", "Text"),
+              Row("num", "Integer"),
+            ),
+          )
         }
 
-      val expected = {
-        Table(
-          Chunk(
-            Row("str", "Text"),
-            Row("num", "Integer", description = Some("it's a number")),
-          ),
-        )
-      }
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+      test("Described") {
+        case class Foo(str: String, num: Int)
+        val conf =
+          (Config.string("str") ++ Config.int("num").??("it's a number")).map { case (str, num) =>
+            Foo(str, num)
+          }
 
-      assertTrue(GenConfigDocs2.gen(conf) == expected)
-    },
-    test("Described") {
-      case class Foo(str: String, num: Int)
-      val conf =
-        (Config.string("str") ++ Config.int("num").??("it's a number")).map { case (str, num) =>
-          Foo(str, num)
+        val expected = {
+          Table(
+            Chunk(
+              Row("str", "Text"),
+              Row("num", "Integer", description = Some("it's a number")),
+            ),
+          )
         }
 
-      val expected = {
-        Table(
-          Chunk(
-            Row("str", "Text"),
-            Row("num", "Integer", description = Some("it's a number")),
-          ),
-        )
-      }
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+      test("Described") {
+        case class Foo(str: String, num: Int)
+        val conf =
+          (Config.string("str") ++ Config.int("num").??("it's a number")).map { case (str, num) =>
+            Foo(str, num)
+          }
 
-      assertTrue(GenConfigDocs2.gen(conf) == expected)
-    },
-    test("Optional") {
-      case class Foo(str: String, num: Option[Int])
-      val conf =
-        (Config.string("str") ++ Config.int("num").optional).map { case (str, num) =>
-          Foo(str, num)
+        val expected = {
+          Table(
+            Chunk(
+              Row("str", "Text"),
+              Row("num", "Integer", description = Some("it's a number")),
+            ),
+          )
         }
 
-      val expected = {
-        Table(
-          Chunk(
-            Row("str", "Text"),
-            Row("num", "Integer", optional = true),
-          ),
-        )
-      }
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+      test("Optional") {
+        case class Foo(str: String, num: Option[Int])
+        val conf =
+          (Config.string("str") ++ Config.int("num").optional).map { case (str, num) =>
+            Foo(str, num)
+          }
 
-      assertTrue(GenConfigDocs2.gen(conf) == expected)
-    },
+        val expected = {
+          Table(
+            Chunk(
+              Row("str", "Text"),
+              Row("num", "Integer", optional = true),
+            ),
+          )
+        }
+
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+    ),
+    suite("variants")(
+      test("Default value") {
+        case class Foo(str: String, num: Int)
+
+        val conf =
+          (Config.string("str") ++ Config.int("num").withDefault(1)).map { case (str, num) =>
+            Foo(str, num)
+          }
+
+        val expected = {
+          Table(
+            Chunk(
+              Row("str", "Text"),
+              Row("num", "Integer", default = Some(1)),
+            ),
+          )
+        }
+
+        assertTrue(GenConfigDocs2.gen(conf) == expected)
+      },
+    ),
   )
 
 }
